5a63c0b94e8d50e24af3a47d37b7f434d7e68ec8
Denis <ndenis@cps.k12.il.us>
2024-09-19 12:42:03 -0500

    made some changes

---
+++ week4_callenge.py
@@ -0,0 +1,19 @@
+
+# ################################Extracting Sub-Strings###################################
+# Extracting Sub-Strings Practice #1
+# Extract the first word of the following sentence using slicing, and display it on the screen:
+ttext=" Controlling complexity is the essence of programming"
+print( ttext [1:12])
+
+# Hint: "Controlling" is 11 characters long.
+
+# Extracting Sub-Strings Practice #2
+# Take every third character starting from the ninth to the end of the sentence, and print the result.
+# "Never trust a computer you can't throw out a window"
+my_text2=" Never trust a computer you can't throw out a window"
+print(my_text2[9::3])
+# Extracting Sub-Strings Practice #3
+# Reverses the position of all the characters in the following sentence and displays the result on the screen.
+texts = " It's great to work with computers. They dont argue, they rember everything and they dont drink vodka "
+# "It's great to work with computers. They don't argue, they remember everything and they don't drink your beer"
+print( texts [::-1]) 
